ccdadb5d88b8811e730e8ea7e52af82dd4ae67a1
Injoker <amitraj.1996@gmail.com>
2018-01-29 10:51:52 +0530

    intial commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	database :'mongodb://localhost:27017/genjour3',
+	database :'mongodb://root:abc123@ds117888.mlab.com:17888/genjour3',
+	//database :'mongodb://localhost:27017/genjour3',
 	secret :'7w47r8fs54s7q99s6d21c477ed96A4s'
 }
